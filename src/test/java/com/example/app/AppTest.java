813c0dfed7444d438da037c128d483a178776cdc
René de Waele <renedewaele@gmail.com>
2025-06-24 14:26:32 +0200

    chore: Upgrade FC client version

--- src/test/java/com/example/app/AppTest.java
+++ src/test/java/com/example/app/AppTest.java
@@ -2,17 +2,14 @@ package com.example.app;
 
 import com.example.app.user.GetUsers;
 import io.fluxcapacitor.javaclient.test.TestFixture;
-import io.fluxcapacitor.javaclient.test.spring.FluxCapacitorTestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.DirtiesContext;
 
 import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
 
 @SpringBootTest
-@Import(FluxCapacitorTestConfig.class)
 @DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
 class AppTest {
 
