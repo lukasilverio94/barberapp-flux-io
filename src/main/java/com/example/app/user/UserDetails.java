29298df2be3ea38083d23649a55126940bec21bb
lukasilverio <lukasilverio94@gmail.com>
2025-08-19 15:31:47 +0200

    (refactor): remove password from userdetails

--- src/main/java/com/example/app/user/UserDetails.java
+++ src/main/java/com/example/app/user/UserDetails.java
@@ -5,7 +5,5 @@ import jakarta.validation.constraints.NotBlank;
 public record UserDetails(
         @NotBlank String name,
         @NotBlank String phone,
-        @NotBlank String email,
-        @NotBlank String password
-        ) {
+        @NotBlank String email) {
 }
