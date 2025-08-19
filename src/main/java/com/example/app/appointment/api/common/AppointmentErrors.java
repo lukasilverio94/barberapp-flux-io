f6cf632d409003b0cbf5bff59ad6ab838efda7d1
lukasilverio <lukasilverio94@gmail.com>
2025-08-19 16:10:20 +0200

    (fix): typo on errors

--- src/main/java/com/example/app/appointment/api/common/AppointmentErrors.java
+++ src/main/java/com/example/app/appointment/api/common/AppointmentErrors.java
@@ -7,7 +7,7 @@ public interface AppointmentErrors {
 
     IllegalCommandException alreadyExists = illegalCommandException("Appointment already exists"),
             notFound = illegalCommandException("Appointment not found"),
-            outOfService = illegalCommandException("Opening time is 8:00 and closing time is 18:00. Closed at Sundayse");
+            outOfService = illegalCommandException("Opening time is 8:00 and closing time is 18:00. Closed at Sundays");
 
 
     static IllegalCommandException illegalCommandException(String message) {
